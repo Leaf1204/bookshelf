0feb9ce143b7764202870c8745c18f5d63cf3bfb
Leanne A Frisinger <67919464+Leaf1204@users.noreply.github.com>
2020-08-23 22:03:35 -0600

    linked to google books API

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 console.log("hello")
+////api
+///https://www.googleapis.com/books/v1/volumes?q=scifi&key=AIzaSyApx2mrOvapaQUu15bI9SnyJyUlalMPJIs
+
+$(()=>{
+    $('#searchButton').on('click', (event)=>{
+        console.log("clicked")
+        
+
+        const searchTerm = $('input[type="text"]').val();
+
+        $.ajax({
+            url:`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=AIzaSyApx2mrOvapaQUu15bI9SnyJyUlalMPJIs`
+        }).then(
+            (data)=>{
+                console.log(data)
+            },
+            ()=>{
+                console.log('bad');
+            }
+        );
+    })
+})
