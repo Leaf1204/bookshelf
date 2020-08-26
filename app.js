7bfa30628b0ec365417a4127d7a2d0b577aeb7ae
Leanne A Frisinger <67919464+Leaf1204@users.noreply.github.com>
2020-08-26 13:15:34 -0600

    data populating the carousel

--- app.js
+++ app.js
@@ -3,24 +3,27 @@ console.log("hello")
 ///https://www.googleapis.com/books/v1/volumes?q=scifi&key=AIzaSyApx2mrOvapaQUu15bI9SnyJyUlalMPJIs
 //https://tympanus.net/codrops/2011/08/16/circular-content-carousel/ link for 
 $(()=>{
+    let currentImgIndex = 0
+    let highestIndex = 0
     $('#searchButton').on('click', (event)=>{
         console.log("clicked")
         
 
         const searchTerm = $('input[type="text"]').val();
+        
 
         $.ajax({
             url:`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=AIzaSyApx2mrOvapaQUu15bI9SnyJyUlalMPJIs`
         }).then(
             (data)=>{
                 console.log(data)
-                ///to do make div per book
-                //loop the data 
-                ///data.items.length to itterate the array
                 for(let i=0; i<data.items.length; i++){
                     const item = data.items[i];
                     const $slide=$(`<div/>`);
                     $slide.addClass('slide');
+                    if (i!=0){
+                        $slide.css("display", "none")
+                    }
                     $('#results').append($slide);
                     const $bookCover = $(`<div/>`);
                     $bookCover.addClass('bookcover');
@@ -52,6 +55,7 @@ $(()=>{
                     $bookInfo.append($summary);
                     let summary = item.volumeInfo.description;
                     $summary.text(summary);
+                    highestIndex = i;
                 }
             },
             ()=>{
@@ -59,4 +63,17 @@ $(()=>{
             }
         );
     })
+   
+        $(".next").on("click", () => {
+  // stuff will go here
+    console.log("Next was clicked");
+    $("#results").children().eq(currentImgIndex).css("display", "none");
+      // Wrap the carousel back to zero if on the last image:
+  if (currentImgIndex < highestIndex) {
+    currentImgIndex++;
+  } else {
+    currentImgIndex = 0;
+  }
+  $("#results").children().eq(currentImgIndex).css("display", "flex");
+});
 })
