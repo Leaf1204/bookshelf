cdd0607699729b0ca1b8ac678822ad0af2b84f52
Leanne A Frisinger <67919464+Leaf1204@users.noreply.github.com>
2020-08-28 13:50:01 -0600

    added book info link

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ $(()=>{
     $('#searchButton').on('click', (event)=>{
         console.log("clicked")
         
-        $('.container2').css("display", "flex");
         $('#results').empty();
         currentImgIndex = 0;
         const searchTerm = $('input[type="text"]').val();
         
+
         /////checkboxes
         let searchUrl = `https://www.googleapis.com/books/v1/volumes?q=subject:${searchTerm}&printType=books&maxResults=40&&orderBy=newest&key=AIzaSyApx2mrOvapaQUu15bI9SnyJyUlalMPJIs`
         
@@ -20,12 +20,23 @@ $(()=>{
         if (filter !== undefined){
             searchUrl+=`&filter=${filter}`
         }
-
+        
         $.ajax({
             url: searchUrl
         }).then(
             (data)=>{
                 console.log(data)
+                
+                if(data.totalItems === 0){
+                    $('#books').css("display", "none");
+                    $('#noBooks').css("display", "flex");
+                    return
+                }else{
+                    $('#books').css("display", "flex");
+                    $('#noBooks').css("display", "none");
+                }
+
+                
                 for(let i=0; i<data.items.length; i++){
                     const item = data.items[i];
                     const $slide=$(`<div/>`);
@@ -68,12 +79,24 @@ $(()=>{
                     $bookInfo.append($summary);
                     if(item.volumeInfo.description !== undefined){
                         let summary = item.volumeInfo.description;
-                        if(summary.length>300) {
-                            $summary.html(`<b>Summary</b>: ${summary.substring(0,300)}...`);
+                        if(summary.length>350) {
+                            $summary.html(`<b>Summary</b>: ${summary.substring(0,350)}...`);
                         }else {
                             $summary.html(`<b>Summary</b>: ${summary}`);
                         }
                     }
+
+                    
+                        const $dateBook = $(`<button id="dateBook${i}"   class="button">what to "date" this book?</button>`);
+                        $summary.append($dateBook);
+                        $(`#dateBook${i}`).on("click", () => {
+                          console.log("date button was clicked");
+                          const dateBook = item.volumeInfo.canonicalVolumeLink;
+                          window.open(dateBook,'_blank' // <- This is what makes it open in a new window.
+                            );
+                            console.log(dateBook);
+                        });
+                    
                     highestIndex = i;
                 }
             },
